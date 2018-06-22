eaca4450534b3c8afea7269f818740ed09943683
manifestlifeinc <iking@lasell.edu>
2018-06-22 06:05:32 -0400

    2

--- App.js
+++ App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import { createStackNavigator } from 'react-navigation';
+import {
+    createStackNavigator
+} from 'react-navigation';
 import Search from './components/Search';
 import Results from './components/Results';
-import { YellowBox } from 'react-native'
+import {
+    YellowBox
+} from 'react-native'
 YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated'])
 
 const Navigator = createStackNavigator({
-        Home: {
-            screen: Search,
-        },
-        Results: {
-            screen: Results,
-        },
+    Home: {
+        screen: Search,
     },
-    {
-        initialRouteName: 'Home',
-    });
+    Results: {
+        screen: Results,
+    },
+}, {
+    initialRouteName: 'Home',
+});
 
 export default class App extends React.Component {
     render() {
-        return <Navigator />;
+        return <Navigator / > ;
     }
 }
-
